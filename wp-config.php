422dfc61100902d2bdaf1d5345b1204ef603d0b6
afzalpatwary <afzalhp2022@gmail.com>
2023-06-14 14:34:51 +0600

    updated

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'apps' );
+define( 'DB_NAME', 'wadi-badi' );
 
 /** Database username */
 define( 'DB_USER', 'root' );

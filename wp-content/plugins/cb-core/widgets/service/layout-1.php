594d9a0aeff58ca32fc3b5966e718cbb5c45f1b2
hemalgetweb <hemalgetweb@gmail.com>
2023-06-14 12:03:48 +0600

    updated

---
+++ wp-content/plugins/cb-core/widgets/service/layout-1.php
@@ -0,0 +1,15 @@
+<!-- service area start -->
+<section class="service-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-12">
+                <div class="apps-service-section-wrapper-114">
+                    <span class="apps-service-section-subtitle-114"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/service-spinner.png" alt="service"> Our Services</span>
+                    <h3 class="apps-service-section-title-114">Full-Cycle eCommerce Solutions</h3>
+                    <p>Offer an amazing shopping experience by leveraging the latest trends and innovation in the eCommerce industry</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- service area end -->

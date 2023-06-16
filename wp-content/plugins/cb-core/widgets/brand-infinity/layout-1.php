0d802ecfe70303baa0cf1acbf561c56b60a36b53
afzalpatwary <afzalhp2022@gmail.com>
2023-06-16 18:35:31 +0600

    brabd section working running

--- wp-content/plugins/cb-core/widgets/brand-infinity/layout-1.php
+++ wp-content/plugins/cb-core/widgets/brand-infinity/layout-1.php
@@ -1,1 +1,103 @@
-<h1>hello from repeater</h1>
+<!--  Happy Clients start -->
+<div class="section-padding bg-clr-deepDark">
+  <div class="section-header text-center">
+    <div class="title-hints d-flex align-items-center justify-content-center gap-2 " data-wow-duration="0.200s"
+      data-wow-delay="100ms">
+      <img src="assets/img/title-process-icon.svg" class="img-fluid" alt="icon">
+      <h4 class="fs-12 fw-bold ls-1 text-clr-primary text-uppercase m-0">2180+ clients</h4>
+    </div>
+    <h2 class="text-white fw-semi-bold fs-36 ">
+      Our happy clients
+    </h2>
+    <p class="fs-6 text-clr-primary2 ">
+      We have helped these brands establish themselves in the UAE’s digital
+      ecosystem. <br class="d-none d-xl-inline"> It’s high time for you to walk the same
+      path to success!
+    </p>
+  </div>
+  <div class="brand-carousel-area my-5">
+    <div class="marquee-vertical">
+      <div class="top-slide-wrap d-flex align-items-center">
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque1.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque2.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque3.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque4.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque5.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque1.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque2.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque3.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque4.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/up-marque5.svg" class="img-fluid" alt="icon">
+        </div>
+      </div>
+    </div>
+
+    <div class="marquee-reverse mt-lg-4 mt-3">
+      <div class="top-slide-wrap d-flex align-items-center">
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque2.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque3.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque4.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque5.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque6.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque2.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque3.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque4.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque5.svg" class="img-fluid" alt="icon">
+        </div>
+        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+          <img src="assets/img/bottom-marque6.svg" class="img-fluid" alt="icon">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="text-center">
+    <div class="d-inline-block">
+      <a class="btn position-relative rounded bg-btn text-uppercase border-0 bg-clr-extraLight text-clr-dark1 fs-14 fw-bold d-flex gap-2 align-items-center "
+        href="client.html">
+        View all Clients
+        <svg class="btn-icon position-absolute" width="10" height="10" viewBox="0 0 10 10" fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path d="M5 10L4.115 9.115L7.60417 5.625H0V4.375H7.60417L4.115 0.885L5 0L10 5L5 10Z" fill="#003C4F" />
+        </svg>
+      </a>
+    </div>
+  </div>
+</div>
+<!--/ Happy Clients end -->

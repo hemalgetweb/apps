68dca0997f1dedb137227468bf8344b2c0c76091
afzalpatwary <afzalhp2022@gmail.com>
2023-06-19 13:29:43 +0600

    heading color settings add

--- wp-content/plugins/cb-core/widgets/brand-infinity/layout-1.php
+++ wp-content/plugins/cb-core/widgets/brand-infinity/layout-1.php
@@ -1,93 +1,38 @@
 <!--  Happy Clients start -->
-<div class="section-padding bg-clr-deepDark">
-  <div class="section-header text-center">
-    <div class="title-hints d-flex align-items-center justify-content-center gap-2 " data-wow-duration="0.200s"
-      data-wow-delay="100ms">
-      <img src="assets/img/title-process-icon.svg" class="img-fluid" alt="icon">
-      <h4 class="fs-12 fw-bold ls-1 text-clr-primary text-uppercase m-0">2180+ clients</h4>
-    </div>
-    <h2 class="text-white fw-semi-bold fs-36 ">
-      Our happy clients
-    </h2>
-    <p class="fs-6 text-clr-primary2 ">
-      We have helped these brands establish themselves in the UAE’s digital
-      ecosystem. <br class="d-none d-xl-inline"> It’s high time for you to walk the same
-      path to success!
-    </p>
-  </div>
-  <div class="brand-carousel-area my-5">
-    <div class="marquee-vertical">
-      <div class="top-slide-wrap d-flex align-items-center">
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque1.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque2.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque3.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque4.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque5.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque1.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque2.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque3.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque4.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/up-marque5.svg" class="img-fluid" alt="icon">
-        </div>
+<div class="section-padding p-0">
+  <div class="brand-carousel-area">
+    <?php if (empty($settings['reverce_direction'])): ?>
+      <div class="marquee-vertical">
+        <?php if (!empty($settings['slides'])): ?>
+          <div class="top-slide-wrap d-flex align-items-center">
+            <?php foreach ($settings['slides'] as $slide): ?>
+              <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+                <?php if (!empty($slide['product_brand_image'])): ?>
+                  <img src="<?php echo esc_url($slide['product_brand_image']['url']); ?>" class="img-fluid" alt="icon">
+                <?php endif; ?>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        <?php endif; ?>
       </div>
-    </div>
-
-    <div class="marquee-reverse mt-lg-4 mt-3">
-      <div class="top-slide-wrap d-flex align-items-center">
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque2.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque3.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque4.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque5.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque6.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque2.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque3.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque4.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque5.svg" class="img-fluid" alt="icon">
-        </div>
-        <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
-          <img src="assets/img/bottom-marque6.svg" class="img-fluid" alt="icon">
-        </div>
+    <?php else: ?>
+      <div class="marquee-reverse mt-lg-4 mt-3">
+        <?php if (!empty($settings['slides'])): ?>
+          <div class="top-slide-wrap d-flex align-items-center">
+            <?php foreach ($settings['slides'] as $slide): ?>
+              <div class="slide-item radius-6 d-flex align-items-center justify-content-center">
+                <?php if (!empty($slide['product_brand_image'])): ?>
+                  <?php echo wp_get_attachment_image($slide['product_brand_image']['id'], 'full'); ?>
+                <?php endif; ?>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        <?php endif; ?>
       </div>
-    </div>
+    <?php endif; ?>
   </div>
 
-  <div class="text-center">
+  <!-- <div class="text-center">
     <div class="d-inline-block">
       <a class="btn position-relative rounded bg-btn text-uppercase border-0 bg-clr-extraLight text-clr-dark1 fs-14 fw-bold d-flex gap-2 align-items-center "
         href="client.html">
@@ -98,6 +43,6 @@
         </svg>
       </a>
     </div>
-  </div>
+  </div> -->
 </div>
 <!--/ Happy Clients end -->

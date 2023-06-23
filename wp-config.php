9e27611b282c042d4e5461808739da45a5fc4a21
hemalgetweb <hemalgetweb@gmail.com>
2023-06-23 17:17:22 +0600

    updated

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wadi-badi');
+define('DB_NAME', 'apps');
 
 /** Database username */
 define('DB_USER', 'root');
@@ -80,7 +80,7 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
 define('WP_DEBUG', true);
-
+define( 'WP_DEBUG_LOG', true );
 /* Add any custom values between this line and the "stop editing" line. */
 
 

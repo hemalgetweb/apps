5bc2d2522146148ce17c7c0aeebf8c0da19d7c4c
afzalpatwary <afzalhp2022@gmail.com>
2023-06-23 19:15:42 +0600

    job post design done

--- wp-content/themes/apps/single-job.php
+++ wp-content/themes/apps/single-job.php
@@ -1,7 +1,137 @@
 <?php
 get_header();
-echo "<br/><br/>";
-the_title();
-the_content();
+?>
+
+<!-- job_post -->
+<section class="job_post section-padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+
+        <div class="job_description mb-5">
+          <h3 class="fs-4 text-clr-dark1 fw-semi-bold mb-4">
+            Key Responsibilities
+          </h3>
+          <ul class="job_key ps-4">
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Liaising with clients to determine their requirements, timescale and budget
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Planning concepts by studying relevant information and materials
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Schedule projects and define budget constraints
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Developing concepts, graphics and layouts, including making decisions about fonts, images, readability and
+              readers’ needs
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Illustrating concepts by designing examples of art arrangement, size, type size and style and submitting
+              them for approval
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Use the appropriate colors and layouts for each graphic
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Creating a wide range of graphics and layouts for product illustrations, company logos, and websites with
+              software such as photoshop
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Keeping clients up to date, listening to and acting on feedback, and explaining the rationale behind
+              graphic design decisions
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Liaising with external printers or production teams to ensure deadlines are met and material is printed or
+              presented to the highest quality
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Amend designs after feedback
+            </li>
+          </ul>
+        </div>
+
+        <div class="job_description mb-5">
+          <h3 class="fs-4 text-clr-dark1 fw-semi-bold mb-4">
+            Skills Required
+          </h3>
+          <ul class="job_key ps-4">
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Bachelor’s degree (or equivalent) in graphic design, art, or related discipline
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Three or more years of experience (academic and professional) with design software, including Illustrator,
+              InDesign, Photoshop, Dreamweaver
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Demonstrable graphic design skills with a strong portfolio
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Organizational and time-management skills for meeting deadlines in a fast-paced environment
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Working knowledge of CSS3, HTML5, and JavaScript
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Knowledge of WordPress and content management systems
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              A strong eye for visual composition
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Excellent communication and presentation skills
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Able to give and receive constructive criticism.
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <div class="col-lg-4">
+        <div class="job_summary p-4 radius-6">
+          <h3 class="fs-4 text-clr-dark1 fw-semi-bold mb-4">
+            Job Summary
+          </h3>
+
+          <ul class="job_key ps-4 mb-4">
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Position --- Graphic Designer/Illustrator
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Employment Type --- Full-time
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Industry --- Marketing
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Experience --- 3+ Years
+
+            </li>
+            <li class="fs-18 fw-normal text-clr-dark2 mb-3">
+              Remote Friendly --- Yes
+            </li>
+          </ul>
+
+          <div class="btn-wrap" data-wow-duration="0.200s" data-wow-delay="400ms">
+            <a class="btn rounded bg-btn text-uppercase border-0 bg-clr-extraLight text-clr-dark1 fs-14 fw-bold d-flex gap-2 align-items-center w-100 text-center d-flex justify-content-center"
+              href="#">
+              Apply now
+              <svg class="btn-icon" width="10" height="10" viewBox="0 0 10 10" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 10L4.115 9.115L7.60417 5.625H0V4.375H7.60417L4.115 0.885L5 0L10 5L5 10Z" fill="#003C4F">
+                </path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--/ job_post -->
+
+
+<?php
 get_footer();
 ?>

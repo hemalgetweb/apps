50c2cdb158da083918af636a0b83b36190fff78c
afzalpatwary <afzalhp2022@gmail.com>
2023-07-18 17:48:47 +0600

    last update from afjal vai

--- wp-content/plugins/apps-job-listing/apps-job-listing.php
+++ wp-content/plugins/apps-job-listing/apps-job-listing.php
@@ -9,16 +9,19 @@
  * Text Domain: apps-job-listing
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH'))
+    exit; // Exit if accessed directly
 define("AJL_PLUGINS_VERSION", time());
 define("AJL_INCLUDE_DIR", plugin_dir_path(__FILE__) . "includes/");
 define("AJL_DATA_DIR", plugin_dir_path(__FILE__) . "data/");
 define("AJL_ADMIN_DIR", plugin_dir_url(__FILE__) . "assets/admin/");
 
-class Apps_Job_Listing {
+class Apps_Job_Listing
+{
     private $version;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->version = AJL_PLUGINS_VERSION;
         add_action("plugins_loaded", [$this, 'apl_plugins_loaded']);
         add_action("admin_enqueue_scripts", [$this, "load_admin_assets"]);
@@ -26,36 +29,40 @@ class Apps_Job_Listing {
         add_action("plugins_loaded", [$this, "get_all_country_list"]);
         register_activation_hook(__FILE__, [$this, 'increase_max_allowed_packet']);
         add_action('admin_enqueue_scripts', [$this, 'enqueue_image_meta_box_scripts']);
-        add_filter( 'comments_open', [$this, 'remove_comment_field_for_page'],  10, 2 );
+        add_filter('comments_open', [$this, 'remove_comment_field_for_page'], 10, 2);
     }
-    function remove_comment_field_for_page($open, $post_id) {
+    function remove_comment_field_for_page($open, $post_id)
+    {
         $page_id = get_option('application_form_page_id');
-        if ( $post_id === $page_id ) {
+        if ($post_id === $page_id) {
             // Disable comments for the specified page
             $open = false;
         }
-    
+
         return $open;
     }
-    function enqueue_image_meta_box_scripts($hook) {
+    function enqueue_image_meta_box_scripts($hook)
+    {
         if ($hook === 'post.php' || $hook === 'post-new.php') {
             wp_enqueue_media();
-            wp_enqueue_script('image-meta-box', AJL_ADMIN_DIR. 'js/application-meta-box.js', array('jquery'), '1.0.0', true);
+            wp_enqueue_script('image-meta-box', AJL_ADMIN_DIR . 'js/application-meta-box.js', array('jquery'), '1.0.0', true);
         }
     }
-    function increase_max_allowed_packet() {
+    function increase_max_allowed_packet()
+    {
         global $wpdb;
-    
+
         // Set the new value for max_allowed_packet
         $new_value = '64M'; // Adjust the value as per your requirement
-        
+
         // Execute the SQL query to update the max_allowed_packet value
         $query = $wpdb->prepare("SET GLOBAL max_allowed_packet=%s", $new_value);
         $wpdb->query($query);
     }
-    function get_all_country_list() {
+    function get_all_country_list()
+    {
         global $apps_country_list; // Declare the global variable
-        $jsonData = file_get_contents(AJL_DATA_DIR. 'all_countries.json');
+        $jsonData = file_get_contents(AJL_DATA_DIR . 'all_countries.json');
         // Check if the data was retrieved successfully
         if ($jsonData !== false) {
             // Decode the JSON data into an associative array
@@ -67,24 +74,27 @@ class Apps_Job_Listing {
             echo "Failed to retrieve JSON data.";
         }
     }
-    function apl_plugins_loaded() {
-        load_plugin_textdomain("apps-job-listing", false, __FILE__."/languages");
+    function apl_plugins_loaded()
+    {
+        load_plugin_textdomain("apps-job-listing", false, __FILE__ . "/languages");
     }
-    
-    function include_required_files() {
-        include_once(AJL_INCLUDE_DIR. 'post-types.php');
-        include_once(AJL_INCLUDE_DIR. 'post-type-application.php');
-        include_once(AJL_INCLUDE_DIR. 'form-handler/application-form-handler.php');
-        include_once(AJL_INCLUDE_DIR. 'shortcodes/application_form.php');
-        include_once(AJL_INCLUDE_DIR. 'meta-box/application-meta-box.php');
+
+    function include_required_files()
+    {
+        include_once(AJL_INCLUDE_DIR . 'post-types.php');
+        include_once(AJL_INCLUDE_DIR . 'post-type-application.php');
+        include_once(AJL_INCLUDE_DIR . 'form-handler/application-form-handler.php');
+        include_once(AJL_INCLUDE_DIR . 'shortcodes/application_form.php');
+        include_once(AJL_INCLUDE_DIR . 'meta-box/application-meta-box.php');
     }
-    
-    function load_admin_assets() {
-        wp_enqueue_style('datepicker', AJL_ADMIN_DIR. 'css/datepicker.css', array(), '1.0.0');
-        wp_enqueue_style('apps-main', AJL_ADMIN_DIR. 'css/apps-main.css', array(), $this->version);
-        wp_enqueue_style('select2', AJL_ADMIN_DIR. 'css/select2.min.css', array(), '1.0.0');
-        wp_enqueue_script('select2', AJL_ADMIN_DIR. 'js/select2.min.js', array('jquery'), '1.0.0', true);
-        wp_enqueue_script('apps-main', AJL_ADMIN_DIR. 'js/main.js', array('jquery', 'jquery-ui-sortable', 'jquery-ui-datepicker'), $this->version, true);
+
+    function load_admin_assets()
+    {
+        wp_enqueue_style('datepicker', AJL_ADMIN_DIR . 'css/datepicker.css', array(), '1.0.0');
+        wp_enqueue_style('apps-main', AJL_ADMIN_DIR . 'css/apps-main.css', array(), $this->version);
+        wp_enqueue_style('select2', AJL_ADMIN_DIR . 'css/select2.min.css', array(), '1.0.0');
+        wp_enqueue_script('select2', AJL_ADMIN_DIR . 'js/select2.min.js', array('jquery'), '1.0.0', true);
+        wp_enqueue_script('apps-main', AJL_ADMIN_DIR . 'js/main.js', array('jquery', 'jquery-ui-sortable', 'jquery-ui-datepicker'), $this->version, true);
     }
 }
 
---
+++ wp-content/plugins/cb-core/widgets/statement/layout-1.php
@@ -0,0 +1,69 @@
+<?php
+
+?>
+
+<!-- statement -->
+<section class="statement ">
+  <div class="map">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/img/map-img.svg" alt="map img" class="img-fluid">
+  </div>
+  <div class="container">
+      <div class="row">
+        <div class="col-lg-7 offset-lg-5 section-padding">
+            <div class="statement-info radius-16 position-relative mx-4 ms-xl-5">
+                <div class="inner-statement p-4 radius-16">
+
+                   <?php if(!empty($settings['statement_title'])) : ?>
+                        <h3 class="statement_title text-white fs-36 fw-bold"><?php echo $settings['statement_title']; ?></h3>
+                    <?php endif; ?>
+
+                
+               
+                    
+                  <?php if (!empty($settings['statement_description'])) : ?>
+                     <p class="statement_description text-clr-dark5 fs-5 fw-normal mb-4">
+                          <?php echo $settings['statement_description']; ?>
+                     </p>
+                  <?php endif; ?>
+
+                
+                  <div class="statement-bottom d-flex align-items-center justify-content-center justify-content-sm-between flex-wrap">
+                      <div class="owner-info d-flex align-items-center gap-2">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/owner.png" alt="owner-img" class="owner-img img-fluid rounded-circle">
+                        <div class="name">
+                          <h4 class="fs-18 text-clr-dark5">
+                           <b> MD Shah Alam</b> — Founder & CEO 
+                          </h4>
+                        </div>
+                      </div>
+                      <div class="owner-sign">
+                           <img src="<?php echo get_template_directory_uri() ?>/assets/img/signatory.svg" alt="owner-img" class="img-fluid flex-shrink-0">
+                      </div>
+                  </div>
+                  <div class="quote-shape">
+                      <img src="<?php echo get_template_directory_uri() ?>/assets/img/quote-left.svg" alt="icon" class="quote-left img-fluid position-absolute">
+                      <img src="<?php echo get_template_directory_uri() ?>/assets/img/quote-right.svg" alt="icon" class="quote-right img-fluid position-absolute">
+                  </div>
+               </div>
+             </div>
+            </div>
+          </div>
+        </div>
+</section>
+<!--/ statement -->
+
+
+
+<?php ?>
+
+
+
+
+
+
+
+  
+
+
+
+

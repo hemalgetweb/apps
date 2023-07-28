d227e7dae499b9cdcd9c7756165daf8aa8255e4b
shorifahammed <shorifwork@gmail.com>
2023-07-28 10:50:05 +0600

    updated

--- wp-content/plugins/cb-core/widgets/heading/layout-1.php
+++ wp-content/plugins/cb-core/widgets/heading/layout-1.php
@@ -7,8 +7,9 @@ $enable_container_class = $settings['enable_container'] ? 'container' : 'apps-ha
         <div class="col-xxl-12">
             <div class="apps-service-section-wrapper-114 text-<?php echo esc_attr($alignment); ?>">
                 <?php if (!empty($settings['heading_subtitle'])): ?>
-                    <span class="apps-service-section-subtitle-114"><img
-                            src="<?php echo get_template_directory_uri(); ?>/assets/img/service-spinner.png" alt="service">
+                    <span class="apps-service-section-subtitle-114">
+                        <img
+                            src="<?php echo get_template_directory_uri(); ?>/assets/img/service-spinner.png" width="10" height="10" alt="service">
                         <?php echo wp_kses_post($settings['heading_subtitle']); ?></span>
                 <?php endif; ?>
                 <?php if (!empty($settings['heading_title'])): ?>

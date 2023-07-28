4b69b8207eaee35b5aaa5d7e054953226689430d
shorifahammed <shorifwork@gmail.com>
2023-07-28 10:48:15 +0600

    updated

--- wp-content/plugins/cb-core/widgets/hero/layout-1.php
+++ wp-content/plugins/cb-core/widgets/hero/layout-1.php
@@ -8,7 +8,8 @@
                         <?php if (!empty($settings['subtitle'])): ?>
                             <span
                                 class="section-tag fs-12 fw-bold text-uppercase text-clr-primary2 d-inline-flex gap-2 align-items-center mb-2 ">
-                                <?php echo wp_get_attachment_image( $settings['subtitle_image']['id'], 'full' ); ?>
+                                <img width="10" height="10" src="<?php echo esc_url($settings['subtitle_image']['url']); ?>" alt="icon"
+                                    class="img-fluid">
                                 <?php echo cb_core_kses_basic($settings['subtitle']); ?>
                             </span>
                         <?php endif; ?>

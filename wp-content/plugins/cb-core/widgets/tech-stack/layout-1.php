bd65a97f6482a6c84a0eae25d8bddc0fc47ee2c2
Ashik <ashikur1993@gmail.com>
2023-08-07 16:05:05 +0600

    Update

--- wp-content/plugins/cb-core/widgets/tech-stack/layout-1.php
+++ wp-content/plugins/cb-core/widgets/tech-stack/layout-1.php
@@ -3,14 +3,14 @@
     <div class="container">
         <div class="tech-stack-box-wrapper-114">
             <div class="row align-items-center">
-                <div class="col-xxl-4 col-xl-4 col-lg-4 text-center text-md-start">
+                <div class="col-xxl-4 col-xl-4 col-lg-4 text-center text-lg-start">
                     <?php if(!empty($settings['tech_stack_title'])) : ?>
                         <h5 class="tech-stack-box-left-label-114"><?php echo esc_html($settings['tech_stack_title']); ?></h5>
                     <?php endif; ?>
                 </div>
                 <div class="col-xxl-8 col-xl-8 col-lg-8">
                     <?php if(!empty($settings['slides'])) : ?>
-                    <div class="tech-stack-box-right-icon-list-114 justify-content-center justify-content-md-start">
+                    <div class="tech-stack-box-right-icon-list-114 justify-content-center justify-content-lg-start">
                         <?php foreach($settings['slides'] as $slide) :  ?>
                         <div class="tech-stack-box-icon-single-114">
                             <?php if(!empty($slide['stack_image']['url'])) : ?>

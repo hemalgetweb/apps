441495ebbb8f0b074d3e2f085aef55a6d9538389
shorifahammed <shorifwork@gmail.com>
2023-08-07 12:33:46 +0600

    updated

--- wp-content/themes/apps/assets/js/script.js
+++ wp-content/themes/apps/assets/js/script.js
@@ -195,7 +195,6 @@ $(window).scroll(function () {
             const sectionHeight = current.offsetHeight;
             const sectionTop = current.offsetTop - 50;
             var sectionId = current.getAttribute("id");
-        console.log(sectionId);
             
             /*
             - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
@@ -205,9 +204,9 @@ $(window).scroll(function () {
             scrollY > sectionTop &&
             scrollY <= sectionTop + sectionHeight
             ){
-            document.querySelector(".apps-scrollable-content-sections-nav-114 ul li a[href*="+sectionId+ "]").classList.add("active");
+            document.querySelector(".apps-scrollable-content-sections-nav-114 ul li a[href*="+sectionId+ "]").addClass("active");
             } else {
-            document.querySelector(".apps-scrollable-content-sections-nav-114 ul li a[href*="+sectionId+"]").classList.remove("active");
+            document.querySelector(".apps-scrollable-content-sections-nav-114 ul li a[href*="+sectionId+"]").removeClass("active");
             }
         });
         }

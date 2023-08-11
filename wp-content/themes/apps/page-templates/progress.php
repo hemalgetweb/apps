cea64170965214fba394e39bb8ee89ade3c90d03
Md. Asraful Hoque <asraful15-6625@diu.edu.bd>
2023-08-11 15:09:27 +0600

    updated

---
+++ wp-content/themes/apps/page-templates/progress.php
@@ -0,0 +1,25 @@
+<?php
+// Template Name: Progress
+get_header();
+?>
+  <div class="wadi-al-bada-progressbar section-padding ">
+        <div class="container">
+            <div class="mt-5">
+                <h3 class="text-center mb-5">section heading</h3>
+            </div>
+            <div class="wadi-work-process">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="wadi-work-process-item">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/process1.svg"
+                        alt="visual-design " class="img-fluid">
+                        <h4 class="process-item-title">Detailed Consultation</h4>
+                        <p class="process-item-pra">When you share your web/app development and marketing needs with us, we craft the best strategy based on your products/services, customers, competition, and more</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php 
+get_footer();

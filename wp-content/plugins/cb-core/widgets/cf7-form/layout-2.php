9f585ab60b5317f1fd47b14d117119ae8059a371
Ashik <ashikur1993@gmail.com>
2023-08-11 12:47:49 +0600

    Update

--- wp-content/plugins/cb-core/widgets/cf7-form/layout-2.php
+++ wp-content/plugins/cb-core/widgets/cf7-form/layout-2.php
@@ -49,7 +49,7 @@
                                 <?php endif;  ?>
 
                                 <?php if(!empty($settings['number_text2'])) : ?>
-                                <a href="tel:<?php echo $settings['number_link2'] ?? $settings['number_link2']; ?>"><img src="http://wadialbadaitsolutions.ae/wp-content/uploads/2023/07/p-1.svg" alt="tel"><?php echo esc_html($settings['number_text2']); ?></a>
+                                <a href="tel:<?php echo $settings['number_link2'] ?? $settings['number_link2']; ?>"><svg width="35" height="34" viewBox="0 0 35 34" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M17.6316 34C10.1462 28.1126 4.26901 22.4312 0 16.9558C4.61988 10.9506 10.4971 5.2987 17.6316 0C24.0058 4.76883 29.7953 10.4208 35 16.9558C32.7193 19.7818 29.9415 22.7844 26.6667 25.9636C23.3918 29.1429 20.3801 31.8216 17.6316 34Z" fill="#FABCC6"/> <path d="M12.2424 23.23V12.302H15.7944L18.0024 19.598L20.1624 12.302H23.7624V23.23H21.5544V15.134H21.3304L19.0424 22.942H16.9624L14.6744 15.134H14.4504V23.23H12.2424Z" fill="#003959"/> </svg><?php echo esc_html($settings['number_text2']); ?></a>
                                 <?php endif;  ?>
                             </div>
                             

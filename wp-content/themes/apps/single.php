da739e5ce23cfd9900a1475d95e83a96e68be29d
shorifahammed <shorifwork@gmail.com>
2023-08-11 19:37:03 +0600

    updated

--- wp-content/themes/apps/single.php
+++ wp-content/themes/apps/single.php
@@ -30,6 +30,20 @@ if(!empty($category)) {
     <!-- blog-details -->
     <section class="blog-details section-padding"
         style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/img/w-shape.svg); background-repeat: no-repeat;">
+		<div class="apps-blog-archive-banner-top-114">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/rect.svg" alt="image"><span class="subtitle">Digital Marketing</span>
+			<h4 class="title">Contrary to popular belief, Lorem Ipsum is not simply random text. It roots in a piece</h4>
+			<div class="meta">
+				<div class="author-left">
+					<img src="" alt="image">
+					<span>Published by John wick</span>
+				</div>
+				<div class="author-right">
+				<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"> <g id="01 align center"> <path id="Vector" d="M17.5 1.66667H15V0H13.3333V1.66667H6.66667V0H5V1.66667H2.5C1.83696 1.66667 1.20107 1.93006 0.732233 2.3989C0.263392 2.86774 0 3.50363 0 4.16667L0 20H20V4.16667C20 3.50363 19.7366 2.86774 19.2678 2.3989C18.7989 1.93006 18.163 1.66667 17.5 1.66667ZM1.66667 4.16667C1.66667 3.94565 1.75446 3.73369 1.91074 3.57741C2.06702 3.42113 2.27899 3.33333 2.5 3.33333H17.5C17.721 3.33333 17.933 3.42113 18.0893 3.57741C18.2455 3.73369 18.3333 3.94565 18.3333 4.16667V6.66667H1.66667V4.16667ZM1.66667 18.3333V8.33333H18.3333V18.3333H1.66667Z" fill="#A8E0FF"/> <path id="Vector_2" d="M14.1667 10.8335H12.5V12.5002H14.1667V10.8335Z" fill="#A8E0FF"/> <path id="Vector_3" d="M10.8346 10.8335H9.16797V12.5002H10.8346V10.8335Z" fill="#A8E0FF"/> <path id="Vector_4" d="M7.49869 10.8335H5.83203V12.5002H7.49869V10.8335Z" fill="#A8E0FF"/> <path id="Vector_5" d="M14.1667 14.1667H12.5V15.8334H14.1667V14.1667Z" fill="#A8E0FF"/> <path id="Vector_6" d="M10.8346 14.1667H9.16797V15.8334H10.8346V14.1667Z" fill="#A8E0FF"/> <path id="Vector_7" d="M7.49869 14.1667H5.83203V15.8334H7.49869V14.1667Z" fill="#A8E0FF"/> </g> </svg>
+				<span>07 jan 2023</span>
+				</div>
+			</div>
+		</div>
         <div class="container mt-0 mt-xl-5 pt-0 pt-xl-4">
             <div class="row">
                 <div class="col-lg-8">

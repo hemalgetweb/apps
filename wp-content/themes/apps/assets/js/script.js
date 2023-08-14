286f71010ecc881612d78f621b47968dadcb85d1
shorifahammed <shorifwork@gmail.com>
2023-08-14 17:00:46 +0600

    updated

--- wp-content/themes/apps/assets/js/script.js
+++ wp-content/themes/apps/assets/js/script.js
@@ -14,18 +14,16 @@
         sections.forEach(current => {
             const sectionHeight = current.offsetHeight;
             const sectionTop = current.offsetTop - 50;
-            sectionId = current.getAttribute("id");
-
-            /*
-            - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-            - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
-            */
+            var sectionId = current.getAttribute("id");
+			console.log('section id: '+sectionId);
             if (
                 scrollY > sectionTop &&
                 scrollY <= sectionTop + sectionHeight
             ) {
+				console.log('add execute');
                 document.querySelector(".apps-scrollable-content-sections-nav-114 nav ul li a[href*=" + sectionId + "]").classList.add("active");
             } else {
+				console.log('delete execute');
                 document.querySelector(".apps-scrollable-content-sections-nav-114 nav ul li a[href*=" + sectionId + "]").classList.remove("active");
             }
         });

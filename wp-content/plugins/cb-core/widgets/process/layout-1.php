6c372724a12e1ddb62cf531524d9648f89bb64ae
shorifahammed <shorifwork@gmail.com>
2023-08-14 15:36:50 +0600

    udpated

--- wp-content/plugins/cb-core/widgets/process/layout-1.php
+++ wp-content/plugins/cb-core/widgets/process/layout-1.php
@@ -16,17 +16,13 @@
             <div class="col-xl-4">
                 <div class="process-item aos-init aos-animate" data-aos="<?php echo $position_aos_fade; ?>">
                     <div class="process-inner ml-auto">
-                    <div class="img-wrapper">
-                        <?php
-                         $process_icon = $slide['process_icon'];
-                         $image_url = wp_get_attachment_image_url($process_icon, 'thumbnail');
-                         $image_alt = get_post_meta($process_icon, '_wp_attachment_image_alt', true);
-                         $image_class = 'process-m-icon';
-                         
-                         if ($image_url) {
-                             echo '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($image_alt) . '" class="' . esc_attr($image_class) . '">';
-                         }
-                        ?>
+                        <div class="img-wrapper">
+                        <?php if(!empty($slide['process_icon_sm']['url'])) : ?>
+                            <img src="<?php echo $slide['process_icon_sm']['url']; ?>" class="process-m-icon" alt="<?php echo  \Elementor\Control_Media::get_image_alt( $slide['process_icon_sm'] ); ?>">
+                        <?php endif; ?>
+                        <?php if(!empty($slide['process_icon']['url'])) : ?>
+                            <img src="<?php echo $slide['process_icon']['url']; ?>" class="process-icon" alt="<?php echo  \Elementor\Control_Media::get_image_alt( $slide['process_icon'] ); ?>">
+                        <?php endif; ?>
                         <?php
                         $process_icon_sm = $slide['process_icon_sm'];
                         $image_url_sm = wp_get_attachment_image_url($process_icon_sm, 'thumbnail');

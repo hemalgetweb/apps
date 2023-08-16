70de7082556912f97cca0eae6e9c3de2e347b390
afzalpatwary <afzalhp2022@gmail.com>
2023-08-16 18:14:59 +0600

    index file update

--- wp-content/themes/apps/index.php
+++ wp-content/themes/apps/index.php
@@ -68,7 +68,7 @@ $categories = get_categories(
 			if ($query->have_posts()):
 				while ($query->have_posts()):
 					$query->the_post(); ?>
-					<div class="col-xl-4 col-sm-6 col-sm-6 mb-4">
+					<div class="col-xl-4 col-md-6 mb-4">
 						<div id="post-<?php the_ID(); ?>" <?php post_class('single-blog bg-white p-2 radius-6 box-shadow2'); ?>>
 							<?php if ((function_exists('has_post_thumbnail')) && (has_post_thumbnail())):
 								$att = get_post_thumbnail_id();

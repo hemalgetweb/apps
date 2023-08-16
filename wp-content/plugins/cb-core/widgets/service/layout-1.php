5314f039fbe535bfa323b9bfb019a91cabe30c4e
shorifahammed <shorifwork@gmail.com>
2023-08-16 11:45:49 +0600

    updated

--- wp-content/plugins/cb-core/widgets/service/layout-1.php
+++ wp-content/plugins/cb-core/widgets/service/layout-1.php
@@ -7,27 +7,25 @@
         <div class="row justify-content-center">
             <?php foreach($settings['slides'] as $slide) : ?>
             <div class="col-xxl-4 col-xl-4 col-md-6 col-sm-6">
-                <div class="apps-service-box-114 mb-30" style="min-height: <?php echo $box_min_height ? $box_min_height. 'px' : ''; ?>">
-                    <?php if(!empty($slide['service_image']['url'])) : ?>
-                    <div class="apps-service-box-icon-114">
-                        <img src="<?php echo esc_url($slide['service_image']['url']); ?>" alt="service">
-                    </div>
-                    <?php endif; ?>
-                    <div class="apps-service-box-content-wrap-114">
-                        <?php if(!empty($slide['service_title'])) : 
-                            if ( ! empty( $slide['service_title_link']['url'] ) ) {
-                                $this->add_link_attributes( 'service_title_link', $slide['service_title_link'] );
-                            }
-                            ?>
-                        <h5 class="apps-service-box-title-114">
-                            <a href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']): ''; ?>"><?php echo wp_kses_post($slide['service_title']);  ?></a>
-                        </h5>
+                <a href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']): ''; ?>">
+                    <div class="apps-service-box-114 mb-30" style="min-height: <?php echo $box_min_height ? $box_min_height. 'px' : ''; ?>">
+                        <?php if(!empty($slide['service_image']['url'])) : ?>
+                        <div class="apps-service-box-icon-114">
+                            <img src="<?php echo esc_url($slide['service_image']['url']); ?>" alt="service">
+                        </div>
                         <?php endif; ?>
-                        <?php if(!empty($slide['service_content'])) : ?>
-                        <p><?php echo wp_kses_post($slide['service_content']); ?></p>
-                        <?php endif; ?>
+                        <div class="apps-service-box-content-wrap-114">
+                            <?php if(!empty($slide['service_title'])) : ?>
+                            <h5 class="apps-service-box-title-114">
+                                <span><?php echo wp_kses_post($slide['service_title']);  ?></span>
+                            </h5>
+                            <?php endif; ?>
+                            <?php if(!empty($slide['service_content'])) : ?>
+                            <p><?php echo wp_kses_post($slide['service_content']); ?></p>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
             <?php endforeach; ?>
         </div>

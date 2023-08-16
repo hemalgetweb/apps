02da5f74ad1de26c1d8379da458dddfbcd21fb86
afzalpatwary <afzalhp2022@gmail.com>
2023-08-16 15:58:11 +0600

    detail template

--- wp-content/themes/apps/page-templates/details-template.php
+++ wp-content/themes/apps/page-templates/details-template.php
@@ -1,18 +1,9 @@
 <?php
-/**
- * The details of the blog post
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package apps
- */
-get_header();
+
+// Template Name: Details template
+
 ?>
+<?php get_header(); ?>
 
 
 <div class="">

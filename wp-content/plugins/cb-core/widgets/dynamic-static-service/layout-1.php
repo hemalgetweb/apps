f3194f034b7b1ca91d436ec44ebef972829eb585
shorifahammed <shorifwork@gmail.com>
2023-08-16 11:33:49 +0600

    updated cb core service link

--- wp-content/plugins/cb-core/widgets/dynamic-static-service/layout-1.php
+++ wp-content/plugins/cb-core/widgets/dynamic-static-service/layout-1.php
@@ -15,27 +15,26 @@ if($settings['card_height']) {
                     <?php foreach($settings['slides'] as $slide) : ?>
                     <!-- single slide -->
                     <div class="swiper-slide">
-                        <div class="dynamic-service-box-114" style="min-height: <?php echo $card_height ? $card_height. ';': ' ;'; ?> <?php echo $card_hide_border ? 'border: 0; box-shadow: 0px 20px 40px 0px rgba(0, 57, 89, 0.10);': ' ;'; ?>">
-                            <div class="dynamic-service-box-img-114">
-                                <?php echo wp_get_attachment_image( $slide['service_image']['id'], 'full' ); ?>
+                        <a href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']): ''; ?>">
+                            <div class="dynamic-service-box-114" style="min-height: <?php echo $card_height ? $card_height. ';': ' ;'; ?> <?php echo $card_hide_border ? 'border: 0; box-shadow: 0px 20px 40px 0px rgba(0, 57, 89, 0.10);': ' ;'; ?>">
+                                <div class="dynamic-service-box-img-114">
+                                    <?php echo wp_get_attachment_image( $slide['service_image']['id'], 'full' ); ?>
+                                </div>
+                                <div class="dynamic-service-box-content-114">
+                                    <?php if(!empty($slide['service_title'])) : ?>
+                                        <h5 class="title"><?php echo wp_kses_post($slide['service_title']); ?></h5>
+                                    <?php endif; ?>
+                                    <?php if(!empty($slide['service_excerpt'])) : ?>
+                                        <p><?php echo wp_kses_post($slide['service_excerpt']); ?></p>
+                                    <?php endif; ?>
+                                    <?php if(!empty($slide['service_title_link']['url'])) : ?>
+                                        <span class="dynamic-service-read-more-btn-114">Read more 
+                                        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 10L4.115 9.115L7.60417 5.625H0V4.375H7.60417L4.115 0.885L5 0L10 5L5 10Z" fill="#00C7C7"/></svg>
+                                    </span>
+                                    <?php endif; ?>
+                                </div>
                             </div>
-                            <div class="dynamic-service-box-content-114">
-                                <?php if(!empty($slide['service_title'])) : ?>
-                                    <h5 class="title"><a href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']): ''; ?>"><?php echo wp_kses_post($slide['service_title']); ?></a></h5>
-                                <?php endif; ?>
-                                <?php if(!empty($slide['service_excerpt'])) : ?>
-                                    <p><?php echo wp_kses_post($slide['service_excerpt']); ?></p>
-                                <?php endif; ?>
-                                <?php if(!empty($slide['service_title_link']['url'])) : ?>
-                                    <a href="<?php echo esc_url($slide['service_title_link']['url']); ?>" class="dynamic-service-read-more-btn-114">Read more 
-                                    <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5 10L4.115 9.115L7.60417 5.625H0V4.375H7.60417L4.115 0.885L5 0L10 5L5 10Z" fill="#00C7C7"/>
-</svg>
-
-                                    </a>
-                                <?php endif; ?>
-                            </div>
-                        </div>
+                        </a>
                     </div>
                     <?php endforeach; ?>
                 </div>

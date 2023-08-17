6f1f7bd06105ce71b1b547d4556494547e520559
shorifahammed <shorifwork@gmail.com>
2023-08-17 14:42:56 +0600

    udpa

--- wp-content/plugins/getweb-gutenberg-block/build/service-card/index.asset.php
+++ wp-content/plugins/getweb-gutenberg-block/build/service-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'df0b3554f69f4885dc6f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '71f1024bab4c4b0a5215');

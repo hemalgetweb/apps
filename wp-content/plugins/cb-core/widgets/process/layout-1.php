e0bdc6b76b9be85e079c6f82d06b54ce897b2ccd
shorifahammed <shorifwork@gmail.com>
2023-08-17 16:35:44 +0600

    updated

--- wp-content/plugins/cb-core/widgets/process/layout-1.php
+++ wp-content/plugins/cb-core/widgets/process/layout-1.php
@@ -5,28 +5,49 @@
             <?php
                 foreach($settings['slides'] as $index => $slide) :
                 $position_aos_fade = "";
-                if($index == 1 || $index == 2 || $index == 3 || $index == 7 || $index == 8 || $index == 9) {
-                    $position_aos_fade = "fade-right";
-                } elseif($index == 1 || $index == 2 || $index == 3) {
-                    $position_aos_fade = "fade-left";
+                $inner_classes = '';
+                switch($index) {
+                    case 0:
+                        $position_aos_fade = "fade-right";
+                        $inner_classes = "ml-auto m-left";
+                        break;
+                    case 1:
+                        $position_aos_fade = "fade-right";
+                        $inner_classes = "margin-auto m-right";
+                        break;
+                    case 2:
+                        $position_aos_fade = "fade-right";
+                        $inner_classes = "mr-auto m-left";
+                        break;
+                    case 3:
+                        $position_aos_fade = "fade-left";
+                        $inner_classes = "ml-auto m-right";
+                        break;
+                    case 4:
+                        $position_aos_fade = "fade-left";
+                        $inner_classes = "margin-auto m-left";
+                        break;
+                    case 5:
+                        $position_aos_fade = "fade-left";
+                        $inner_classes = "mr-auto m-right";
+                        break;
+                    case 6:
+                        $position_aos_fade = "fade-right";
+                        $inner_classes = "ml-auto m-left";
+                        break;
+                    case 7:
+                        $position_aos_fade = "fade-right";
+                        $inner_classes = "margin-auto m-right";
+                        break;
+                    case 8:
+                        $position_aos_fade = "fade-right";
+                        $inner_classes = "mr-auto m-left";
+                        break;
                 }
-                $ml_class = '';
-                $ml_auto_class = '';
-                $margin_auto = '';
-                if($index == 0 || $index == 2 || $index == 4 || $index == 6 || $index == 8) {
-                    $ml_class = 'm-left';
-                }
-                if($index == 0 || $index == 3 || $index == 6) {
-                    $ml_auto_class = 'ml-auto';
-                }
-                if($index == 1 || $index == 4 || $index == 7) {
-                    $margin_auto = 'margin-auto';
-                }
-
             ?>
             <div class="col-xl-4 col-md-6">
-                <div class="process-item aos-init aos-animate" data-aos="<?php echo $position_aos_fade; ?>">
-                    <div class="process-inner <?php echo esc_attr($ml_class); ?> <?php echo esc_attr($ml_auto_class); ?> <?php echo esc_attr($margin_auto); ?>">
+                <div class="process-item" data-aos="<?php echo $position_aos_fade; ?>">
+                    <div class="process-inner <?php echo esc_attr($inner_classes); ?>">
                         <div class="img-wrapper">
                         <?php if(!empty($slide['process_icon_sm']['url'])) : ?>
                             <img src="<?php echo $slide['process_icon_sm']['url']; ?>" class="process-m-icon" alt="<?php echo  \Elementor\Control_Media::get_image_alt( $slide['process_icon_sm'] ); ?>">

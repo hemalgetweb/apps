2631253b740568e7484c9a45ee8366ad196c06a4
ashikrahman-dev <ashikur1993@gmail.com>
2023-08-24 19:10:40 +0600

    Update

--- wp-content/plugins/cb-core/widgets/project-tab/layout-1.php
+++ wp-content/plugins/cb-core/widgets/project-tab/layout-1.php
@@ -1,31 +1,3 @@
-<div class="modal fade apps-portfolio-modal" id="modal_for_project" tabindex="-1" aria-labelledby="modal_for_projectLabel" aria-hidden="true">
-    <div class="container">
-        <div class="apps-portfolio-modal-top-114  pt-40">
-            <div class="apps-portfolio-moal-top-left-box">
-                <button type="button" class="btn-close apps-portfolio-close" data-bs-dismiss="modal"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M24 1.414L22.586 0L12 10.586L1.414 0L0 1.414L10.586 12L0 22.586L1.414 24L12 13.414L22.586 24L24 22.586L13.414 12L24 1.414Z" fill="white"/>
-</svg>
-</button>
-                <div class="icon">
-                    <img src="https://wadialbadaitsolutions.ae/wp-content/uploads/2023/08/portfolio-details.svg" alt="icon">
-                </div>
-                <div class="content">
-                    <h5 class="title">Easpa – Mobile Wallet App</h5>
-                    <div class="meta">
-                        <p>Wadi Al Bada</p>
-                        <a href="#">Follow</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content bg-transparent">
-            <img src="#" alt="image">
-        </div>
-    </div>
-</div>
-
 <!-- tab project start -->
 <section class="tab-project">
     <div class="container  container-tab-project-114">
@@ -84,16 +56,16 @@
                                 }
                                 $project_big_image = get_post_meta($post_id, 'project_image', true);
                                 ?>
-                                <div class="col col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                                <div class="col col-xl-4 col-lg-4 col-md-6 col-sm-12">
                                     <div class="apps-project-tab-main-content-114 mb-30">
                                         <!-- project card -->
-                                        <div class="apps-project-card-114 apps-has-portfolio-popup cursor-pointer" data-bs-toggle="modal" data-bs-target="#modal_for_project" data-url="<?php echo $project_big_image ? esc_url($project_big_image): ''; ?>" data-title="<?php echo get_the_title(); ?>">
+                                        <div class="apps-project-card-114">
                                             <?php if(has_post_thumbnail(get_the_ID())) : ?>
                                             <div class="apps-project-card-image-114">
                                                 <?php the_post_thumbnail(get_the_ID(), 'full'); ?>
                                             </div>
                                             <?php endif; ?>
-                                            <div class="apps-project-card-description-wrapper-114"  data-url="<?php echo $project_big_image ? esc_url($project_big_image): ''; ?>" data-bs-toggle="modal" data-bs-target="#modal_for_project" data-title="<?php echo get_the_title(); ?>" >
+                                            <div class="apps-project-card-description-wrapper-114">
                                                 <div class="row">
                                                     <div class="col-xxl-10 col-xl-10 mb-30 mb-xl-0">
                                                         <h5 class="apps-project-card-title-114"><button data-bs-toggle="modal" data-bs-target="#modal_for_project" data-url="<?php echo $project_big_image ? esc_url($project_big_image): ''; ?>" class="apps-has-portfolio-popup" data-title="<?php echo get_the_title(); ?>"><?php echo get_the_title(); ?></button></h5>
@@ -187,7 +159,7 @@
                             <div class="col col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
                                 <div class="apps-project-tab-main-content-114 mb-30">
                                     <!-- project card -->
-                                    <div class="apps-project-card-114 apps-has-portfolio-popup cursor-pointer" data-bs-toggle="modal" data-bs-target="#modal_for_project" data-url="<?php echo $project_big_image ? esc_url($project_big_image): ''; ?>" data-title="<?php echo get_the_title(); ?>">
+                                    <div class="apps-project-card-114">
                                         <?php if(has_post_thumbnail(get_the_ID())) : ?>
                                         <div class="apps-project-card-image-114">
                                             <?php the_post_thumbnail(get_the_ID(), 'full'); ?>
@@ -196,7 +168,9 @@
                                         <div class="apps-project-card-description-wrapper-114">
                                             <div class="row">
                                                 <div class="col-xxl-10 col-xl-10 mb-30 mb-xl-0">
-                                                    <h5 class="apps-project-card-title-114"><button class="apps-has-portfolio-popup"><?php echo get_the_title(); ?></button></h5>
+                                                    <a href="<?php echo get_the_permalink(); ?>" class="apps-project-card-title-114">
+                                                        <?php echo get_the_title(); ?>
+                                                    </a>
                                                     <?php if(!empty($cat_name)) : ?>
                                                         <a href="#0" class="apps-project-card-category-114"><?php echo esc_html($cat_name) ?></a>
                                                     <?php endif; ?>

19424950512ed88c8c7f126b44883b18355e8207
ashikrahman-dev <ashikur1993@gmail.com>
2023-08-30 17:54:23 +0600

    Update

--- wp-content/themes/apps/inc/templates/footer/footer-1.php
+++ wp-content/themes/apps/inc/templates/footer/footer-1.php
@@ -10,6 +10,9 @@ $cbtoolkit_footer_top_repeater = get_theme_mod('cbtoolkit_footer_top_repeater', 
 $contact_page_id = '1802';
 $current_page_id = get_the_ID();
 $contact_space_top = $contact_page_id == $current_page_id ? 'pt-100' : 'pt-300';
+if (is_singular('project')) {
+    $contact_space_top = "pt-100";
+}
 /*
 cmt_section_footer_2: start section Footer 1
 */
@@ -29,10 +32,9 @@ EOD;
     <a target="_blank" href="https://wa.me/+971586130632"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/whatsapp.svg" alt="instagram"></a>
 </div>
 <!-- footer -->
-<footer class="footer bg-clr-deepDark <?php echo esc_attr($contact_space_top); ?>"
-    style="<?PHP echo $bg_properties; ?>">
+<footer class="footer bg-clr-deepDark <?php echo esc_attr($contact_space_top); ?>" style="<?PHP echo $bg_properties; ?>">
     <div class="container">
-        <?php if (is_active_sidebar('footer-1') || is_active_sidebar('footer-2') || is_active_sidebar('footer-3') || is_active_sidebar('footer-4')): ?>
+        <?php if (is_active_sidebar('footer-1') || is_active_sidebar('footer-2') || is_active_sidebar('footer-3') || is_active_sidebar('footer-4')) : ?>
             <div class="footer-widget-area">
                 <div class="row">
                     <?php
@@ -48,7 +50,7 @@ EOD;
                 </div>
             </div>
         <?php endif; ?>
-        <?php if (!empty($cbtoolkit_copyright_1)): ?>
+        <?php if (!empty($cbtoolkit_copyright_1)) : ?>
             <div class="copyright d-flex flex-column flex-sm-row justify-content-center py-3">
                 <p class="m-0 text-clr-dark5 fw-normal fs-14 text-center">
                     <?php echo esc_html($cbtoolkit_copyright_1); ?>

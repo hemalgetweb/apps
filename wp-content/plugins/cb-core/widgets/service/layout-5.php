34e594e4d8b10cf404eacb3dac924136f0feeb6c
ashikrahman-dev <ashikur1993@gmail.com>
2023-08-31 13:14:38 +0600

    Update

--- wp-content/plugins/cb-core/widgets/service/layout-5.php
+++ wp-content/plugins/cb-core/widgets/service/layout-5.php
@@ -8,7 +8,7 @@
                 <?php foreach ($settings['slides'] as $slide) : ?>
                     <div class="col-xxl-4 col-xl-4 col-md-6 col-sm-6">
                      
-                            <a class="wb-seo-feature-item-123" href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']) : ''; ?>">
+                            <a class="wb-seo-feature-item-123 d-block text-decoration-none" href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']) : ''; ?>">
                             <div class="seo-feature-item">
                                 <div class=" mb-30" style="min-height: <?php echo $box_min_height ? $box_min_height . 'px' : ''; ?>">
                                     <?php if (!empty($slide['service_image']['url'])) : ?>

9a289f793b691914695c2e249c501cc31cdede73
ashikrahman-dev <ashikur1993@gmail.com>
2023-08-31 13:03:25 +0600

    Update

--- wp-content/plugins/cb-core/widgets/service/layout-5.php
+++ wp-content/plugins/cb-core/widgets/service/layout-5.php
@@ -8,9 +8,9 @@
                 <?php foreach ($settings['slides'] as $slide) : ?>
                     <div class="col-xxl-4 col-xl-4 col-md-6 col-sm-6">
                         <div class="service-box-card-wrap">
-                            <a class="service-box-card-wrap" href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']) : ''; ?>">
+                            <a class="apps-service-box-114" href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']) : ''; ?>">
 
-                                <div class="apps-service-box-114 mb-30" style="min-height: <?php echo $box_min_height ? $box_min_height . 'px' : ''; ?>">
+                                <div class=" mb-30" style="min-height: <?php echo $box_min_height ? $box_min_height . 'px' : ''; ?>">
                                     <?php if (!empty($slide['service_image']['url'])) : ?>
                                         <div class="apps-service-box-icon-114">
                                             <img src="<?php echo esc_url($slide['service_image']['url']); ?>" alt="service">

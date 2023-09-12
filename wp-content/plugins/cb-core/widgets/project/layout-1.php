9401533913c13b6f640d58f49f136488e27c0d67
Md. Asraful Hoque <asraful15-6625@diu.edu.bd>
2023-09-12 12:52:18 +0600

    ancor added

--- wp-content/plugins/cb-core/widgets/project/layout-1.php
+++ wp-content/plugins/cb-core/widgets/project/layout-1.php
@@ -14,44 +14,46 @@
                 ?>
                     <div class="col-xl-4 col-md-6 col-sm-12">
                         <!-- project card -->
-                        <div class="apps-project-card-114">
-                            <?php if (has_post_thumbnail(get_the_ID())) : ?>
-                                <div class="apps-project-card-image-114">
-                                    <?php the_post_thumbnail(get_the_ID(), 'full'); ?>
-                                </div>
-                            <?php endif; ?>
-                            <div class="apps-project-card-description-wrapper-114">
-                                <div class="row">
-                                    <div class="col-xxl-10 col-xl-10 mb-30 mb-xl-0">
-                                        <a href="<?php echo get_the_permalink(); ?>" class="apps-project-card-title-114">
-                                            <?php echo get_the_title(); ?>
-                                        </a>
-                                        <?php
-                                        if ($categories && !is_wp_error($categories)) {
-                                            $first_category = reset($categories); // Get the first category for the current post
-
-                                            if ($first_category) {
-                                                $cat_name = $first_category->name;
-                                                $cat_link = get_term_link($first_category, $taxonomy); // Get the category link
-                                                echo '<div class="apps-project-card-category-114">' . esc_html($cat_name) . '</div>';
-                                            }
-                                        }
-                                        ?>
+                        <a href="<?php echo get_the_permalink(); ?>">
+                            <div class="apps-project-card-114">
+                                <?php if (has_post_thumbnail(get_the_ID())) : ?>
+                                    <div class="apps-project-card-image-114">
+                                        <?php the_post_thumbnail(get_the_ID(), 'full'); ?>
                                     </div>
-                                    <div class="col-xxl-2 col-xl-2">
-                                        <div class="apps-project-card-action-icon-114 text-xl-end">
-                                            <button class="apps-has-portfolio-popup">
-                                                <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow-top-right.svg" loading="async" width="20" height="20" alt="project"> -->
+                                <?php endif; ?>
+                                <div class="apps-project-card-description-wrapper-114">
+                                    <div class="row">
+                                        <div class="col-xxl-10 col-xl-10 mb-30 mb-xl-0">
+                                            <h4 class="apps-project-card-title-114">
+                                                <?php echo get_the_title(); ?>
+                                            </h4>
+                                            <?php
+                                            if ($categories && !is_wp_error($categories)) {
+                                                $first_category = reset($categories); // Get the first category for the current post
 
-                                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M5.06199 14.707L4.29199 13.937L12.938 5.29103H5.20799V4.20703H14.792V13.791H13.708V6.06103L5.06199 14.707Z" fill="#73A7C3"/>
-                                                </svg>
-                                            </button>
+                                                if ($first_category) {
+                                                    $cat_name = $first_category->name;
+                                                    $cat_link = get_term_link($first_category, $taxonomy); // Get the category link
+                                                    echo '<div class="apps-project-card-category-114">' . esc_html($cat_name) . '</div>';
+                                                }
+                                            }
+                                            ?>
+                                        </div>
+                                        <div class="col-xxl-2 col-xl-2">
+                                            <div class="apps-project-card-action-icon-114 text-xl-end">
+                                                <button class="apps-has-portfolio-popup">
+                                                    <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow-top-right.svg" loading="async" width="20" height="20" alt="project"> -->
+
+                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M5.06199 14.707L4.29199 13.937L12.938 5.29103H5.20799V4.20703H14.792V13.791H13.708V6.06103L5.06199 14.707Z" fill="#73A7C3"/>
+                                                    </svg>
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                 <?php endwhile; ?>
             <?php endif; ?>

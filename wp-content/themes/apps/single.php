527edcd4c83e6a1789815b0ae35ad8b426b31e21
shorifahammed <shorifwork@gmail.com>
2023-09-13 13:00:25 +0600

    udpated

--- wp-content/themes/apps/single.php
+++ wp-content/themes/apps/single.php
@@ -15,6 +15,8 @@ if($cbblog_layout == 'right-sidebar') {
 } else if($cbblog_layout == 'left-sidebar') {
 	$sidebar_space = is_active_sidebar( 'blog-sidebar' ) ? 'pr-50' : '';
 }
+$post_url = get_permalink();
+$post_title = get_the_title();
 $category = array();
 $cat_id = '';
 $cat_name = '';
@@ -115,10 +117,15 @@ $related_posts_query = new WP_Query($related_posts_args);
                                     <h4 class="fs-5 fw-bold text-clr-dark1 mb-0">
                                         <?php echo esc_html__('Share with the world', 'apps'); ?>
                                     </h4>
-                                    <?php echo do_shortcode('[Sassy_Social_Share]') ?>
-
+                                    <div class="apps-blog-details-social-share-links-114">
+                                        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo esc_url($post_url); ?>" target="_blank" class="facebook">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 17.9895 4.3882 22.954 10.125 23.8542V15.4688H7.07812V12H10.125V9.35625C10.125 6.34875 11.9166 4.6875 14.6576 4.6875C15.9701 4.6875 17.3438 4.92188 17.3438 4.92188V7.875H15.8306C14.34 7.875 13.875 8.80008 13.875 9.75V12H17.2031L16.6711 15.4688H13.875V23.8542C19.6118 22.954 24 17.9895 24 12Z" fill="#AECBD3"/></svg>
+                                        </a>
+                                    </div>
+                                    <a href="https://twitter.com/intent/tweet?url=<?php echo esc_url($post_url); ?>&text=<?php echo esc_attr($post_title); ?>" class="twitter" target="_blank"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_6057_111578)"><path d="M7.55016 21.7497C16.6045 21.7497 21.5583 14.2465 21.5583 7.74162C21.5583 7.53068 21.5536 7.31505 21.5442 7.10412C22.5079 6.40722 23.3395 5.54401 24 4.55505C23.1025 4.95436 22.1496 5.21514 21.1739 5.32849C22.2013 4.71266 22.9705 3.74523 23.3391 2.60552C22.3726 3.17831 21.3156 3.58237 20.2134 3.80037C19.4708 3.01132 18.489 2.48887 17.4197 2.31381C16.3504 2.13874 15.2532 2.32081 14.2977 2.83185C13.3423 3.3429 12.5818 4.15446 12.1338 5.14107C11.6859 6.12767 11.5754 7.23437 11.8195 8.29005C9.86249 8.19185 7.94794 7.68346 6.19998 6.79785C4.45203 5.91225 2.90969 4.66919 1.67297 3.14927C1.0444 4.233 0.852057 5.5154 1.13503 6.73585C1.418 7.95629 2.15506 9.0232 3.19641 9.71974C2.41463 9.69492 1.64998 9.48444 0.965625 9.10568V9.16662C0.964925 10.3039 1.3581 11.4063 2.07831 12.2865C2.79852 13.1667 3.80132 13.7703 4.91625 13.9947C4.19206 14.1929 3.43198 14.2218 2.69484 14.0791C3.00945 15.0572 3.62157 15.9126 4.44577 16.5261C5.26997 17.1395 6.26512 17.4804 7.29234 17.501C5.54842 18.8709 3.39417 19.6139 1.17656 19.6104C0.783287 19.6098 0.390399 19.5857 0 19.5382C2.25286 20.9835 4.87353 21.7511 7.55016 21.7497Z" fill="#AECBD3"/></g><defs><clipPath id="clip0_6057_111578"><rect width="24" height="24" fill="white"/></clipPath></defs></svg> </a>
+                                    <a href="https://www.linkedin.com/shareArticle?url=<?php echo esc_url($post_url); ?>&title=<?php echo esc_attr($post_title); ?>" class="linkedin" target="_blank"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.2234 0H1.77187C0.792187 0 0 0.773438 0 1.72969V22.2656C0 23.2219 0.792187 24 1.77187 24H22.2234C23.2031 24 24 23.2219 24 22.2703V1.72969C24 0.773438 23.2031 0 22.2234 0ZM7.12031 20.4516H3.55781V8.99531H7.12031V20.4516ZM5.33906 7.43438C4.19531 7.43438 3.27188 6.51094 3.27188 5.37187C3.27188 4.23281 4.19531 3.30937 5.33906 3.30937C6.47813 3.30937 7.40156 4.23281 7.40156 5.37187C7.40156 6.50625 6.47813 7.43438 5.33906 7.43438ZM20.4516 20.4516H16.8937V14.8828C16.8937 13.5562 16.8703 11.8453 15.0422 11.8453C13.1906 11.8453 12.9094 13.2937 12.9094 14.7891V20.4516H9.35625V8.99531H12.7687V10.5609H12.8156C13.2891 9.66094 14.4516 8.70938 16.1813 8.70938C19.7859 8.70938 20.4516 11.0813 20.4516 14.1656V20.4516Z" fill="#AECBD3"/></svg> </a>
+                                    <a href="mailto:?subject=<?php echo esc_attr($post_title); ?>&body=<?php echo esc_url($post_url); ?>" target="_blank" class="email"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.6942 15.03C12.4675 15.1717 12.2125 15.2284 11.9858 15.2284C11.7591 15.2284 11.5041 15.1717 11.2774 15.03L0 8.14453V17.2968C0 19.252 1.58678 20.8387 3.54191 20.8387H20.4581C22.4132 20.8387 24 19.252 24 17.2968V8.14453L12.6942 15.03Z" fill="#AECBD3"/><path d="M20.4619 3.16016H3.54576C1.87398 3.16016 0.457211 4.35024 0.117188 5.93702L12.018 13.1909L23.8905 5.93702C23.5505 4.35024 22.1337 3.16016 20.4619 3.16016Z" fill="#AECBD3"/></svg></a>
                                 </div>
-                                <?php //get_template_part( 'post-formates/single-post/content', 'biography' ); ?>
                             </div>
                         </div>
                     </div>

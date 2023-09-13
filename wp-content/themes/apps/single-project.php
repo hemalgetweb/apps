419d2377adb6e2cc095b37d0cd6230c654c0343f
shorifahammed <shorifwork@gmail.com>
2023-09-13 11:49:50 +0600

    udpate

--- wp-content/themes/apps/single-project.php
+++ wp-content/themes/apps/single-project.php
@@ -294,7 +294,7 @@ $process_gallery_images = function_exists("get_field") ? get_field("process_gall
         <!-- swiper-control -->
         <div class="swiper-control">
           <div class="swiper-pagination113 d-none"></div>
-          <div class="related-post-arrow">
+          <!-- <div class="related-post-arrow">
             <button type="button" class="swiper-prev case-project-next-114 swiper-arrow border-0">
               <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5 10L5.885 9.115L2.39583 5.625H10V4.375H2.39583L5.885 0.885L5 0L0 5L5 10Z" fill="#73A7C3" />
@@ -305,7 +305,7 @@ $process_gallery_images = function_exists("get_field") ? get_field("process_gall
                 <path d="M5 0L4.115 0.885L7.60417 4.375L0 4.375L0 5.625L7.60417 5.625L4.115 9.115L5 10L10 5L5 0Z" fill="#73A7C3" />
               </svg>
             </button>
-          </div>
+          </div> -->
         </div>
         <!--/ swiper-control -->
       <?php endif; ?>

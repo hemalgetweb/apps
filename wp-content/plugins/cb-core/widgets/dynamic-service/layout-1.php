534704723fb30f6ca3d57772a442622460f959c4
ashikrahman-dev <ashikur1993@gmail.com>
2023-09-15 16:24:52 +0600

    Update

--- wp-content/plugins/cb-core/widgets/dynamic-service/layout-1.php
+++ wp-content/plugins/cb-core/widgets/dynamic-service/layout-1.php
@@ -18,22 +18,22 @@ if($settings['card_height']) {
                     ?>
                     <div class="swiper-slide">
                         <a href="<?php echo get_the_permalink(get_the_ID()); ?>">
-                            <div class="dynamic-service-box-114" style="min-height: <?php echo $card_height ? $card_height. ';': ' ;'; ?> <?php echo $card_hide_border ? 'border: 0; box-shadow: 0px 20px 40px 0px rgba(0, 57, 89, 0.10);': ' ;'; ?>">
-                                <?php if(has_post_thumbnail(get_the_ID())) : ?>
-                                <div class="dynamic-service-box-img-114">
-                                    <?php the_post_thumbnail(get_the_ID(), 'full'); ?>
-                                </div>
-                                <?php endif; ?>
-                                <div class="dynamic-service-box-content-114 dynamic-service-box-content-129">
-                                    <div class="">
+                            <div class="dynamic-service-box-114 dynamic-service-box-129" style="min-height: <?php echo $card_height ? $card_height. ';': ' ;'; ?> <?php echo $card_hide_border ? 'border: 0; box-shadow: 0px 20px 40px 0px rgba(0, 57, 89, 0.10);': ' ;'; ?>">
+                                <div class="">
+                                    <?php if(has_post_thumbnail(get_the_ID())) : ?>
+                                    <div class="dynamic-service-box-img-114">
+                                        <?php the_post_thumbnail(get_the_ID(), 'full'); ?>
+                                    </div>
+                                    <?php endif; ?>
+                                    <div class="dynamic-service-box-content-114">
                                         <h5 class="title"><?php echo get_the_title(get_the_ID()); ?></h5>
                                         <p><?php echo get_the_excerpt(); ?></p>
                                     </div>
-                                    <div class="">
-                                        <span class="dynamic-service-read-more-btn-114"><?php echo esc_html__('Read more', 'cb-core'); ?>
-                                            <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 10L4.115 9.115L7.60417 5.625H0V4.375H7.60417L4.115 0.885L5 0L10 5L5 10Z" fill="#00C7C7"/></svg>
-                                        </span>
-                                    </div>
+                                </div>
+                                <div class="">
+                                    <span class="dynamic-service-read-more-btn-114"><?php echo esc_html__('Read more', 'cb-core'); ?>
+                                        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 10L4.115 9.115L7.60417 5.625H0V4.375H7.60417L4.115 0.885L5 0L10 5L5 10Z" fill="#00C7C7"/></svg>
+                                    </span>
                                 </div>
                             </div>
                         </a>
--- wp-content/plugins/cb-core/widgets/dynamic-static-service/layout-1.php
+++ wp-content/plugins/cb-core/widgets/dynamic-static-service/layout-1.php
@@ -16,12 +16,12 @@ if($settings['card_height']) {
                     <!-- single slide -->
                     <div class="swiper-slide">
                         <a class="service-box-card-wrap d-block" href="<?php echo $slide['service_title_link']['url'] ? esc_url($slide['service_title_link']['url']): ''; ?>">
-                            <div class="dynamic-service-box-114" style="min-height: <?php echo $card_height ? $card_height. ';': ' ;'; ?> <?php echo $card_hide_border ? 'border: 0; box-shadow: 0px 20px 40px 0px rgba(0, 57, 89, 0.10);': ' ;'; ?>">
-                                <div class="dynamic-service-box-img-114">
-                                    <?php echo wp_get_attachment_image( $slide['service_image']['id'], 'full' ); ?>
-                                </div>
-                                <div class="dynamic-service-box-content-114  dynamic-service-box-content-129">
-                                    <div class="">
+                            <div class="dynamic-service-box-114 dynamic-service-box-129" style="min-height: <?php echo $card_height ? $card_height. ';': ' ;'; ?> <?php echo $card_hide_border ? 'border: 0; box-shadow: 0px 20px 40px 0px rgba(0, 57, 89, 0.10);': ' ;'; ?>">
+                                <div class="">
+                                    <div class="dynamic-service-box-img-114">
+                                        <?php echo wp_get_attachment_image( $slide['service_image']['id'], 'full' ); ?>
+                                    </div>
+                                    <div class="dynamic-service-box-content-114  dynamic-service-box-content-129">
                                         <?php if(!empty($slide['service_title'])) : ?>
                                             <h5 class="title"><?php echo wp_kses_post($slide['service_title']); ?></h5>
                                         <?php endif; ?>
@@ -29,14 +29,13 @@ if($settings['card_height']) {
                                             <p><?php echo wp_kses_post($slide['service_excerpt']); ?></p>
                                         <?php endif; ?>
                                     </div>
-
-                                    <div>
-                                        <?php if(!empty($slide['service_title_link']['url'])) : ?>
-                                            <span class="dynamic-service-read-more-btn-114">Read more 
-                                            <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 10L4.115 9.115L7.60417 5.625H0V4.375H7.60417L4.115 0.885L5 0L10 5L5 10Z" fill="#00C7C7"/></svg>
-                                        </span>
-                                        <?php endif; ?>
-                                    </div>
+                                </div>
+                                <div>
+                                    <?php if(!empty($slide['service_title_link']['url'])) : ?>
+                                        <span class="dynamic-service-read-more-btn-114">Read more 
+                                        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 10L4.115 9.115L7.60417 5.625H0V4.375H7.60417L4.115 0.885L5 0L10 5L5 10Z" fill="#00C7C7"/></svg>
+                                    </span>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </a>
